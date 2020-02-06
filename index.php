ada1a0e84b72a9dfcf824b610bdfd9df04e936ec
glebkalachev <megas781@gmail.com>
2020-02-06 16:24:03 +0300

    tree interface implemented

--- index.php
+++ index.php
@@ -6,8 +6,15 @@ require_once 'app-start.php';
 ?>
 
 <?php require SITE_ROOT . 'master-page/Header/header.php'; ?>
-
-<h1>Здесь будет контент</h1>
-
+<div class="column _flex-centering">
+    <h3>Вход в систему</h3>
+    <form action="" method="post">
+        <div><label for="login"></label></div>
+        <div><input type="text" id="login" name="login"></div>
+        <div><label for="password"></label></div>
+        <div><input type="password" id="password" name="password"></div>
+        <div><input type="submit" value="Войти"></div>
+    </form>
+</div>
 <?php require SITE_ROOT . 'master-page/Footer/footer.php' ?>
 
---
+++ tree.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once 'app-start.php';
+
+
+?>
+
+<?php require SITE_ROOT . 'master-page/Header/header.php'; ?>
+<div class="profile-menu">
+    <span class="profile-menu__username">megas781</span>
+    <a href="" class="profile-menu__exit-button">Выйти</a>
+</div>
+<div class="column _flex-centering">
+    <h3>Файлы системы</h3>
+    <div class="tree">
+
+        <div class=" folder">
+            <div class="tree__item-view folder__name">folder_1</div>
+            <div class="folder__items">
+                <a href="" class="tree__item-view file">file1.html</a>
+                <a href="" class="tree__item-view file">file2.html</a>
+                <a href="" class="tree__item-view file">file3.html</a>
+                <div class="folder">
+                    <div class="tree__item-view folder__name">folder_1</div>
+                    <div class="folder__items">
+                        <a href="" class="tree__item-view file">file1.html</a>
+                        <a href="" class="tree__item-view file">file2.html</a>
+                        <a href="" class="tree__item-view file">file3.html</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="" class="tree__item-view file">file1.html</a>
+        <a href="" class="tree__item-view file">file2.html</a>
+        <a href="" class="tree__item-view file">file3.html</a>
+    </div>
+</div>
+
+<script>
+    document.querySelectorAll('.folder__name').forEach(function (value, key, parent) {
+        value.addEventListener('click', function (e) {
+            value.parentElement.classList.toggle('expanded');
+        });
+    });
+</script>
+<?php require SITE_ROOT . 'master-page/Footer/footer.php' ?>
+
